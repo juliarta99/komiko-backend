2a9a88ea8baf77264759b7a6746b9ec276a191d3
juliarta99 <adij4255@gmail.com>
2025-01-16 01:38:35 +0800

    feat: genre controller

--- src/controller/genre.controller.js
+++ src/controller/genre.controller.js
@@ -2,13 +2,8 @@ const { load } = require("cheerio");
 const { fetchPage } = require('../utils/fetchPage');
 const { responseService } = require('../utils/response');
 
-module.exports.all = async (req, res) => {
+module.exports.getAll = async (req, res) => {
     try{
-        let responseBody = responseService.error(
-            'Something Went Wrong',
-            'Invalid Parameter'
-        )
-
         const url = "https://komikstation.co/manga/list-mode/";
         const html = await fetchPage(url);
         const $ = load(html);
@@ -25,8 +20,106 @@ module.exports.all = async (req, res) => {
         });
 
         responseBody = responseService.success(
-            "All Genre",
+            "Get Data All Genre Successfully!",
             genres
+        )
+        res.status(200).json(responseBody);
+    } catch(err) {
+        console.error("Error fetching data:", err.message);
+        const responseBody = responseService.internalServerError("Failed to fetch data");
+        res.status(500).json(responseBody);
+    }
+}
+
+module.exports.getById = async (req, res) => {
+    const { genreId } = req.params;
+    const url = `https://komikstation.co/genres/${genreId}`;
+
+    try {
+        const html = await fetchPage(url);
+        const $ = load(html);
+        const allSeries = [];
+    
+        $(".bs").each((i, element) => {
+            const series = {};
+            const bsx = $(element).find(".bsx");
+        
+            series.title = bsx.find("a").attr("title");
+            series.url = bsx.find("a").attr("href");
+            series.image = bsx.find("img").attr("src");
+            series.latestChapter = bsx.find(".epxs").text();
+            series.rating = bsx.find(".numscore").text();
+        
+            allSeries.push(series);
+        });
+    
+        const pagination = [];
+        $(".pagination a.page-numbers").each((i, element) => {
+            const pageUrl = $(element).attr("href");
+            const pageNumber = $(element).text();
+            pagination.push({ pageUrl, pageNumber });
+        });
+    
+        const nextPage = $(".pagination a.next.page-numbers").attr("href");
+    
+        const responseBody = responseService.success(
+            "Get Data Genre Successfully!",
+            {
+                allSeries: allSeries,
+                pagination: pagination,
+                nextPage: nextPage
+            }
+        )
+        res.status(200).json(responseBody);
+    } catch (err) {
+        console.error("Error fetching data:", err.message);
+        const responseBody = responseService.internalServerError("Failed to fetch data");
+        res.status(500).json(responseBody);
+    }
+}
+
+module.exports.getByIdAndPage = async (req, res) => {
+    const { genreId, page } = req.params;
+    const url = `https://komikstation.co/genres/${genreId}/page/${page}`;
+
+    try{
+        const html = await fetchPage(url);
+        const $ = load(html);
+        const allSeries = [];
+
+        $(".bs").each((i, element) => {
+            const series = {};
+            const bsx = $(element).find(".bsx");
+        
+            series.title = bsx.find("a").attr("title");
+            series.url = bsx.find("a").attr("href");
+            series.image = bsx.find("img").attr("src");
+            series.latestChapter = bsx.find(".epxs").text();
+            series.rating = bsx.find(".numscore").text();
+        
+            allSeries.push(series);
+        });
+    
+        const pagination = [];
+        $(".pagination a.page-numbers").each((i, element) => {
+            const pageText = $(element).text().trim().toLowerCase();
+        
+            if (pageText !== "« sebelumnya" && pageText !== "berikutnya »") {
+                const pageUrl = $(element).attr("href");
+                const pageNumber = $(element).text();
+                pagination.push({ pageUrl, pageNumber });
+            }
+        });
+
+        const nextPage = $(".pagination a.next.page-numbers").attr("href");
+
+        const responseBody = responseService.success(
+            "Get Data Genre Successfully!",
+            {
+                allSeries: allSeries,
+                pagination: pagination,
+                nextPage: nextPage
+            }
         )
         res.status(200).json(responseBody);
     } catch(err) {
--- src/routes/genre.routes.js
+++ src/routes/genre.routes.js
@@ -2,6 +2,8 @@ const router = require('express').Router()
 const genreController = require('../controller/genre.controller')
 
 // routes
-router.get('/all', genreController.all);
+router.get('/all', genreController.getAll);
+router.get('/:genreId', genreController.getById);
+router.get('/:genreId/page/:page', genreController.getByIdAndPage);
 
 module.exports = router
